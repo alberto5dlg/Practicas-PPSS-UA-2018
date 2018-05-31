d7de6fb24f194bf59102cbd09e88da2d3757b533
Alberto de Lucas Gomez <alberto5dlg@gmail.com>
2018-05-31 11:17:41 +0200

    Practica 11 Ejercicio 1

---
+++ P11/practica11/src/test/java/ejercicio1/HandleStringsTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ppss
+ */
+public class HandleStringsTest {
+    
+    public HandleStringsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testC1() {
+        String s = "Hola:quetal:estas";
+        String rEsperado = "quetal";
+        HandleStrings test  = new HandleStrings(); 
+        
+        String rReal = test.extractMiddle(s);
+        Assert.assertEquals(rEsperado, rReal);
+    }
+    
+}
